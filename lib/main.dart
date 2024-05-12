6feaade4c112a0dfc3b5e7d35e1307d17f09f061
Chris M. <crismartin994@gmail.com>
2024-05-12 14:15:38 -0400

    Implemented Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,16 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'app.dart';
 import 'firebase_options.dart';
 
+// const clientId = 'YOUR_CLIENT_ID';
+
 void main() async {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(const MyApp());
 }
